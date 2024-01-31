ac07b2f1791b6b949a53be01237bd8861c04b83e
Michael J. Wilson <mw9568@princeton.edu>
2024-01-31 12:19:01 -0500

    add example typing

--- python/foundation/tools.py
+++ python/foundation/tools.py
@@ -1,21 +1,20 @@
-@profile
-def calculate_pi():
+def calculate_pi(n: int) -> float:
     result = 0.0
 
     # Initialize denominator
     k = 1
- 
-    # Initialize sum 
+
+    # Initialize sum
     for i in range(1_000_000):
         # even index elements are positive
         if i % 2 == 0:
-            result += 4/k
+            result += 4 / k
         else:
             # odd index elements are negative
-            result -= 4/k
-     
-    return result
+            result -= 4 / k
+
+    return round(result, n)
 
 
 if __name__ == "__main__":
-    calculate_pi()
+    calculate_pi(3)
